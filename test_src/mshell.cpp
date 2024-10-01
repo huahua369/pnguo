db7ce5873ad4ea99a146600cc4268af7f5e2ca6c
华仔 <188665600@qq.com>
2024-10-01 22:50:55 +0800

    x

--- test_src/mshell.cpp
+++ test_src/mshell.cpp
@@ -2693,7 +2693,36 @@ namespace hz {
 
 }
 // !hz
-// 
+// win32 color
+/*
+{
+	"name" : "Campbell",
+
+	"cursorColor": "#FFFFFF",
+	"selectionBackground": "#FFFFFF",
+
+	"background" : "#0C0C0C",
+	"foreground" : "#CCCCCC",
+
+	"black" : "#0C0C0C",
+	"blue" : "#0037DA",
+	"cyan" : "#3A96DD",
+	"green" : "#13A10E",
+	"purple" : "#881798",
+	"red" : "#C50F1F",
+	"white" : "#CCCCCC",
+	"yellow" : "#C19C00",
+	"brightBlack" : "#767676",
+	"brightBlue" : "#3B78FF",
+	"brightCyan" : "#61D6D6",
+	"brightGreen" : "#16C60C",
+	"brightPurple" : "#B4009E",
+	"brightRed" : "#E74856",
+	"brightWhite" : "#F2F2F2",
+	"brightYellow" : "#F9F1A5"
+},
+*/
+ 
 //Xterm 256 color dictionary 
 // bgra
 static uint32_t xc[256] = {
