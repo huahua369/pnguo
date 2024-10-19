319fd26623a1a072a38f0d6feee8663c91ab16ed
华仔 <188665600@qq.com>
2024-10-20 05:53:43 +0800

    tree

--- test_src/main_vk.cpp
+++ test_src/main_vk.cpp
@@ -431,7 +431,7 @@ int main()
 			//vkd->load_gltf(R"(E:\app\tools\pnguo\out\bin\media\Cauldron-Media\buster_drone\busterDrone.gltf)", { 12,1,1 }, 1.0);
 			//vkd->load_gltf(R"(E:\model\lets_go_to_the_beach_-_beach_themed_diorama.glb)", { 0,0,20 }, 1.0);
 			////vkd->load_gltf( R"(E:\model\hero_alice_lobby.glb)");
-			//vkd->load_gltf(R"(E:\model\pale_radiance_tree.glb)", { 15,0,-8 }, 1.0);
+			vkd->load_gltf(R"(E:\model\pale_radiance_tree.glb)", { 15,0,-8 }, 1.0);
 			//vkd->load_gltf(R"(E:\model\ka-2000__scx2800-2_cranes (1).glb)", { 5,0,-8 }, 1.0);
 			//load_gltf(vkd, R"(E:\model\maple_trees.glb)");
 
