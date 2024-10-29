5387cf2e5180828a62b03dfc8faf6a23f351cf82
华仔 <188665600@qq.com>
2024-10-29 12:23:38 +0800

    dev

--- test_src/main_vk.cpp
+++ test_src/main_vk.cpp
@@ -413,8 +413,11 @@ int main()
 
 	form_x* form0 = (form_x*)new_form(app, wtitle, ws.x, ws.y, -1, -1, ef_vulkan | ef_resizable /*| ef_borderless*/ | ef_transparent);
 	//form_x* form1 = (form_x*)new_form(app, wtitle1, ws.x, ws.y, -1, -1, ef_vulkan | ef_resizable);
-
 	auto sdldev = form0->get_dev();		// 获取SDL渲染器的vk设备
+	auto kd = sdldev.vkdev;
+	sdldev.vkdev = 0;
+
+	std::vector<uint64_t> phydev = enumeratephysicaldevices(sdldev.inst);
 	vkdg_cx* vkd = new_vkdg(&sdldev);	// 创建vk渲染器 
 	//vkdg_cx* vkd1 = new_vkdg(&sdldev);	// 创建vk渲染器  
 	//SetWindowDisplayAffinity((HWND)form0->get_nptr(), WDA_MONITOR);// 反截图
