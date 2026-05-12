ceaf766f39f2078e3220db1b300c5aa7a932eb5b
华仔 <188665600@qq.com>
2026-05-12 23:32:26 +0800

    增加模糊cs

---
+++ src/pnguo/shaders/agauss_cs.comp.glsl
@@ -0,0 +1,53 @@
+#version 450
+#extension GL_ARB_compute_shader  : enable
+layout(local_size_x = 16, local_size_y = 16) in; //工作组大小适配16x16线程块
+
+layout(set = 0, binding = 0, rgba8) readonly uniform image2D srcImage; //保留采样器
+layout(set = 0, binding = 1, rgba8) readonly uniform image2D targetImage;// 目标底图
+layout(set = 0, binding = 2, rgba8) writeonly uniform image2D dstImage; //输出目标
+
+layout(push_constant, std140) uniform uPushConstant {
+	int filterMode;
+	int glowMode;
+	float glowFactor;
+	vec2 horizontalDir;
+	vec2 verticalDir;
+} pc;
+
+#define NUMWT 9
+float Gauss[NUMWT] = { 0.93, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1 };
+#define WT_NORMALIZE (1.0/(1.0+2.0*(0.93 + 0.8 + 0.7 + 0.6 + 0.5 + 0.4 + 0.3 + 0.2 + 0.1)))
+
+vec4 fpGaussianPass(vec2 tc) {
+	vec4 c = imageLoad(srcImage, ivec2(tc * imageSize(srcImage))) * WT_NORMALIZE;
+	vec2 step = (pc.filterMode == 1) ? pc.verticalDir : pc.horizontalDir;
+	vec2 dir = step;
+	for (int i = 0; i < 9; i++) {
+		c += imageLoad(srcImage, ivec2(tc + dir)) * Gauss[i] * WT_NORMALIZE;
+		c += imageLoad(srcImage, ivec2(tc - dir)) * Gauss[i] * WT_NORMALIZE;
+		dir += step;
+	}
+	if (pc.filterMode == 1) c *= pc.glowFactor;  //垂直模式增强发光
+	return c;
+}
+vec4 fpFinalCompositing(vec2 tc)
+{
+	vec4 c = imageLoad(targetImage, ivec2(tc * imageSize(targetImage)));
+	vec4 sc = imageLoad(srcImage, ivec2(tc * imageSize(srcImage)));
+	if (pc.glowMode == 1) {             //外发光
+		c = 1 - (1 - c) * (1 - sc);     //滤色混合（屏幕）
+		c -= sc;                  //去掉中间
+	}
+	else {                    //内发光
+		c *= sc;                  //正片叠底删除多余像素
+	}
+	return c;
+}
+void main() {
+	ivec2 size = imageSize(srcImage);
+	vec2 tc = vec2(gl_GlobalInvocationID.xy) / vec2(size);
+	vec4 result = (pc.filterMode < 2)
+		? fpGaussianPass(tc)
+		: fpFinalCompositing(tc);
+	imageStore(dstImage, ivec2(tc), result); //写入计算结果
+}
--- src/pnguo/shaders/base_compile.bat
+++ src/pnguo/shaders/base_compile.bat
@@ -2,3 +2,5 @@ glslangValidator base3d.frag.glsl -V --vn base3d_frag -o base3d.frag.h
 glslangValidator base3d.vert.glsl -V --vn base3d_vert -o base3d.vert.h
 glslangValidator base3d2.frag.glsl -V --vn base3d2_frag -o base3d2.frag.h
 glslangValidator base3d2.vert.glsl -V --vn base3d2_vert -o base3d2.vert.h
+glslangValidator agauss_cs.comp.glsl -V --vn agauss_cs_comp -o agauss_cs.comp.h
+aglslc --target-env=vulkan1.3 -fshader-stage=compute -fentry-point=main agauss_cs.comp.glsl -o agauss_cs.spv.h
